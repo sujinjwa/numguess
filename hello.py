1e5836ae69ebdbd29d4d4e679c2d4b7534748814
sujinjwa <nasujin744@naver.com>
2023-01-09 16:36:36 +0900

    feat: Make one random number between 1 and 100

--- hello.py
+++ hello.py
@@ -1,5 +1,10 @@
+import random
+
 username = input('이름을 입력해주세요: ')
 print('Hello, ', username)
 
 answer = input('유추한 정답을 입력해주세요: ')
 print('유추한 값: ', answer)
+
+real_answer = random.randrange(1, 100 + 1)
+print('실제 정답: ', real_answer)
