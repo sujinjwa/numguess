0f4c7d09f112141b2b28d66548772c61cff0010f
sujinjwa <nasujin744@naver.com>
2023-01-10 15:08:56 +0900

    feat: Compare user's guess with answer
    만약 유저가 입력한 값이 정답과 같다면 축하메시지 출력한다.
    그렇지 않은 경우 중,
    유저의 값이 정답보다 크다면 크다는 메시지를,
    유저의 값이 정답보다 작다면 작다는 메시지를 출력한다.

--- hello.py
+++ hello.py
@@ -1,13 +1,30 @@
-import random
+from random import randint
+from time import sleep
 
 username = input('이름을 입력해주세요: ')
 print('Hello, ', username)
 
-answer = input('유추한 정답을 입력해주세요: ')
+# user's guess
+answer = int(input('유추한 정답을 입력해주세요: '))
 print('유추한 값: ', answer)
 
-real_answer = random.randrange(1, 100 + 1)
-print('실제 정답: ', real_answer)
+# create answer
+real_answer = randint(1, 100)
+#print('실제 정답: ', real_answer)
+
+# compare answer with user's guess
+if answer==real_answer:
+    print('*****')
+    sleep(1)
+    print('*****')
+    sleep(1)
+    print('*****')
+    sleep(1)
+    print(f'You got it right!! The answer is {answer}\n')
+elif answer > real_answer: 
+    print(f'That was too high, {username}..')
+elif answer < real_answer:
+    print(f'That was too low, {username}..')
 
 # DOn't Do
 animals = ['cat', 'dog', 'dragon']
