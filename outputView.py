ef427fdb7cab408228f70dd802f7573cbcd83efc
sujinjwa <nasujin744@naver.com>
2023-01-11 16:49:48 +0900

    feat: create outputView.py for printing messages
    i want to send some messages to user

---
+++ outputView.py
@@ -0,0 +1,6 @@
+class OutputView:
+    def printHello(self, name):
+        print(f"Hello, {name}. Let's start!")
+    
+    def printResult(isSucceeded):
+        print(f"Awesome! You got it!" if isSucceeded else f"Oh, that's not the answer..")
