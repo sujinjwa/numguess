b2d9eab238835114542e637e1f66de1b32907539
sujinjwa <nasujin744@naver.com>
2023-01-11 10:02:48 +0900

    feat: count success if  user didn't change his door

--- monty_hall.py
+++ monty_hall.py
@@ -2,6 +2,13 @@ from random import shuffle, choice
 
 doors = [0, 0, 1]
 
-shuffle(doors)
+win_count = 0
 
-user = choice(doors)
+for _ in range(100000):
+    shuffle(doors)
+
+    user = choice(doors)
+    
+    # 사용자가 선택지를 바꾸지 않은 경우
+    if user == 1:
+        win_count += 1
