f698d41e140fe1160455b5ba533140892a891731
sujinjwa <nasujin744@naver.com>
2023-01-11 16:50:54 +0900

    feat: read user's name and say hello

--- numGuess.py
+++ numGuess.py
@@ -1,38 +1,54 @@
+import inputView
+import outputView
+
 from random import randint
 from time import sleep
 
-username = input('이름을 입력해주세요: ')
-print('Hello, ', username)
+class Game:
+    def __init__(self):
+        self.inputView = inputView.InputView()
+        self.outputView = outputView.OutputView()
+        self.name = ''
 
-# user's guess
-answer = int(input('유추한 정답을 입력해주세요: '))
-print('유추한 값: ', answer)
+    def start(self):
+        self.name = self.inputView.read_name()
+        self.outputView.printHello(self.name)
 
-# create answer
-real_answer = randint(1, 100)
-#print('실제 정답: ', real_answer)
+game = Game()
+game.start()
 
-# compare answer with user's guess
-if answer==real_answer:
-    print('*****')
-    sleep(1)
-    print('*****')
-    sleep(1)
-    print('*****')
-    sleep(1)
-    print(f'You got it right!! The answer is {answer}\n')
-elif answer > real_answer: 
-    print(f'That was too high, {username}..')
-elif answer < real_answer:
-    print(f'That was too low, {username}..')
+# username = input('이름을 입력해주세요: ')
+# print('Hello, ', username)
 
-# DOn't Do
-animals = ['cat', 'dog', 'dragon']
+# # user's guess
+# answer = int(input('유추한 정답을 입력해주세요: '))
+# print('유추한 값: ', answer)
 
-# Do (Trailing comma)
-menus = [
-    'ramen',
-    'pasta',
-    'bear soup',
-    'salad',
-]
+# # create answer
+# real_answer = randint(1, 100)
+# #print('실제 정답: ', real_answer)
+
+# # compare answer with user's guess
+# if answer==real_answer:
+#     print('*****')
+#     sleep(1)
+#     print('*****')
+#     sleep(1)
+#     print('*****')
+#     sleep(1)
+#     print(f'You got it right!! The answer is {answer}\n')
+# elif answer > real_answer: 
+#     print(f'That was too high, {username}..')
+# elif answer < real_answer:
+#     print(f'That was too low, {username}..')
+
+# # DOn't Do
+# animals = ['cat', 'dog', 'dragon']
+
+# # Do (Trailing comma)
+# menus = [
+#     'ramen',
+#     'pasta',
+#     'bear soup',
+#     'salad',
+# ]
