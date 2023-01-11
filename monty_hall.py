073a0791a29991bbc30bd0cdcd7833925685b608
sujinjwa <nasujin744@naver.com>
2023-01-11 10:07:13 +0900

    feat: calculate success percentage and show that

--- monty_hall.py
+++ monty_hall.py
@@ -12,3 +12,9 @@ for _ in range(100000):
     # 사용자가 선택지를 바꾸지 않은 경우
     if user == 1:
         win_count += 1
+
+win_percentage = round((100000 - win_count) / 100000 * 100, 2)
+
+print('사용자가 선택지를 변경하지 않은 경우 성공 횟수: ', win_count)
+print('사용자가 선택지를 변경한 경우 성공 횟수:', 100000 - win_count)
+print('선택지를 변경할 경우 성공할 확률: ', win_percentage, '%')
