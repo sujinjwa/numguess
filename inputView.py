696ea2d6a53a9ebc6502aa07f5d8d0a39b6e54d1
sujinjwa <nasujin744@naver.com>
2023-01-11 16:48:38 +0900

    feat: create InputView class for reading user's input

---
+++ inputView.py
@@ -0,0 +1,12 @@
+class InputView:
+    def read_name(self):
+        name = input('Please enter your name: ')
+        return name
+    
+    def read_user_answer():
+        user_answer = input('Please enter your answer: ')
+        return user_answer
+    
+    def read_difficulty():
+        difficulty = input('Please select game difficulty among high/medium/low: ')
+        return difficulty
